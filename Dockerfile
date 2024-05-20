8bd3cb2a9dfce338a678446a3b04dec31adc4c78
ankit-pn <miankitsingh@gmail.com>
2024-05-20 15:18:35 +0530

    feat: init

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy the current directory contents into the container at /usr/src/app
+COPY . .
+
+# Install any needed packages
+RUN pip install --no-cache-dir psutil requests
+
+# Run monitor_processes.py when the container launches
+CMD ["python", "./monitor_processes.py"]
---
+++ monitor_processes.py
@@ -0,0 +1,43 @@
+import psutil
+import time
+import json
+import requests
+from datetime import datetime
+
+def monitor_high_resource_usage():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    high_resource_processes = []
+
+    for proc in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info', 'cmdline']):
+        try:
+            memory_usage_mb = proc.info['memory_info'].rss / 1024**2
+            full_command = ' '.join(proc.info['cmdline']) if proc.info['cmdline'] else ''
+
+            if proc.info['cpu_percent'] > 100 or memory_usage_mb > 1024:
+                process_info = {
+                    "time": current_time,
+                    "pid": proc.info['pid'],
+                    "cpu_percent": proc.info['cpu_percent'],
+                    "memory_usage_mb": memory_usage_mb,
+                    "name": proc.info['name'],
+                    "cmdline": full_command
+                }
+                high_resource_processes.append(process_info)
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+
+    if high_resource_processes:
+        data = {
+            "timestamp": current_time,
+            "high_resource_processes": high_resource_processes
+        }
+        try:
+            response = requests.post("http://localhost:8111/notify_1", json=data)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to send data: {e}")
+
+# Run the monitoring function every minute
+while True:
+    monitor_high_resource_usage()
+    time.sleep(60)
